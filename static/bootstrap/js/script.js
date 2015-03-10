c4b60d6be9b54d255cafe3d6dfb84d09010ba368
joajida <tennogh@hotmail.fr>
2015-03-11 00:23:07 +0100

    Added an animated babyface

--- static/bootstrap/js/script.js
+++ static/bootstrap/js/script.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
     $('div').click(function() {
     	$(this).children('audio').get(0).play();
+    	$(this).children('div').toggleClass('box_rotate');
     });
 });
 
